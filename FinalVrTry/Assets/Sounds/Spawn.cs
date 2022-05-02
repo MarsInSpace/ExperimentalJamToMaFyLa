7ba62faea3b4dbe97c5d338b9a217d10e659b20d
Finescam <s0577242@htw-berlin.de>
2022-05-02 13:29:42 +0200

    3D Audio Plugin
    better sound qually (hopefully)

--- FinalVrTry/Assets/Sounds/Spawn.cs
+++ FinalVrTry/Assets/Sounds/Spawn.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
+    [SerializeField] GameObject Player;
     [SerializeField] List<GameObject> SoundSources = new List<GameObject>();
+    public List<GameObject> currentSounds = new List<GameObject>();
+    public GameObject currentSound;
 
     Vector3 pos = new Vector3(0, 0, 0);
     Quaternion rot = new Quaternion(0, 0, 0, 0);
 
     private void Start()
     {
-        Instantiate(SoundSources[Random.Range(0, SoundSources.Count)], pos, rot);
+        if (currentSounds.Count == 0)
+        {
+            currentSound = Instantiate(SoundSources[Random.Range(0, SoundSources.Count)], pos, rot);
+            currentSounds.Add(currentSound);
+        }
     }
 }
