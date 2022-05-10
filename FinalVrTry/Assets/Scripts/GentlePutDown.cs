383c5f0024439714bb54dad3b2cb20eeb5da636b
unknown <s0577242@htw-berlin.de>
2022-05-10 16:02:28 +0200

    Gentle Fix?
    null check abfuck. Dont ask.

--- FinalVrTry/Assets/Scripts/GentlePutDown.cs
+++ FinalVrTry/Assets/Scripts/GentlePutDown.cs
@@ -4,21 +4,82 @@ using UnityEngine;
 
 public class GentlePutDown : MonoBehaviour
 {
+    PickUpSound pickUp;
+    Explosion explosion;
     YeetSound yeet;
     public float maxVelocity;
     Rigidbody rb;
 
+    GameObject justGrabbedL;
+    GameObject justGrabbedR;
+
     private void Start()
     {
+        pickUp = FindObjectOfType<PickUpSound>().gameObject.GetComponent<PickUpSound>();
+        explosion = FindObjectOfType<Explosion>().gameObject.GetComponent<Explosion>();
         rb = gameObject.GetComponent<Rigidbody>();
         yeet = gameObject.GetComponent<YeetSound>();
     }
 
     private void Update()
     {
-        if(yeet.velocity < maxVelocity)
+        CheckVelLeft();
+        CheckVelRight();
+
+        //if(yeet.velocity < maxVelocity)
+        //{
+        //    rb.velocity = Vector3.zero;
+        //}
+    }
+
+    void CheckVelLeft()
+    {
+        if (justGrabbedL == null)
         {
-            rb.velocity = Vector3.zero;
+            if (pickUp.useingLeftHand)
+            {
+                justGrabbedL = pickUp.grabbedL;
+            }
+        }
+        else
+        {
+            if (explosion.contrVelLeft <= maxVelocity)
+            {
+                if (pickUp.useingLeftHand == false)
+                {
+                    if (justGrabbedL != null)
+                    {
+                        justGrabbedL.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                        justGrabbedL = null;
+                    }
+                }
+            }
         }
     }
+
+    void CheckVelRight()
+    {
+        if (justGrabbedR == null)
+        {
+            if (pickUp.useingRightHand)
+            {
+                justGrabbedR = pickUp.grabbedR;
+            }
+        }
+        else
+        {
+            if (explosion.contrVelRight <= maxVelocity)
+            {
+                if (pickUp.useingRightHand == false)
+                {
+                    if (justGrabbedR != null)
+                    {
+                        justGrabbedR.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                        justGrabbedR = null;
+                    }
+                }
+            }
+        }
+    }
+
 }
